cfbecb7b0d61a47e3eab1057a052cbc36014551e
Mauricio Cardozo <mcardozo@URUDATA.COM>
2022-03-16 16:26:12 -0300

    Se agrega el pryecto

---
+++ RegenerateCover.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RegenerateCover.aspx.cs" Inherits="RegenerateCover_RegenerateCover" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
+    <link rel="stylesheet" href="Styles/Loading.css" />
+    <link href="Styles/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body class="container-fluid">
+
+
+    <div class="row" style="border-bottom: solid 1px grey">
+        <h2 class="col-xs-12 text-center">Regenerar Carátulas</h2>
+    </div>
+
+    <form id="form1" runat="server" class="row text-center aling-items-center" style="margin-top: 40px">
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager>
+        </div>
+        
+        <asp:UpdateProgress ID="UpdateProgress1" runat="server" AssociatedUpdatePanelID="UpdatePanel1">
+            <ProgressTemplate>
+                 <div class="modal2">
+                    <div class="center2">
+                        <img alt="" src="Images/squares.gif" />
+                    </div>
+                 </div>
+            </ProgressTemplate>
+        </asp:UpdateProgress>
+
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:Label runat="server" Style="color: black; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10pt;">Ingrese un número de expediente:&nbsp;&nbsp;</asp:Label>
+                <asp:TextBox runat="server" ID="nroExpediente" Width="15%" Height="25px" placeholder="Ej. 0001/2017"></asp:TextBox>
+                <asp:Button runat="server" ID="btnSubmit2" OnClientClick="return validateAndHidde()" OnClick="SubmitRegenerate_Click" Text="Regenerar" />
+                
+                <div id="messageContainer" runat="server" class="row text-center aling-items-center" style="margin-top: 40px;">
+                    <h3 runat="server" id="alertError" class="alert alert-danger text-center col-xs-8" role="alert" style="margin-left: 16.66666667%; display:none;"></h3>
+                    <h3 runat="server" id="alertSucces" class="alert alert-success text-center col-xs-8" role="alert" style="margin-left: 16.66666667%; display: none;">La carátula ha sido regenerada.</h3>
+                    <h3 id="alertErrorClient" class="alert alert-danger text-center col-xs-8" role="alert" style="margin-left: 16.66666667%; display:none;"> Ingrese un numero de expediente.</h3>
+                </div>
+
+            </ContentTemplate>
+        </asp:UpdatePanel>
+
+       
+    </form>
+
+    <script type="text/javascript">
+        function validateAndHidde() {
+            let txtValue = document.getElementById('<%=nroExpediente.ClientID%>').value;
+            let alertError = document.getElementById('<%=alertError.ClientID%>');
+            let alertSucces = document.getElementById('<%=alertSucces.ClientID%>');
+            alertSucces.style.display = "none";
+
+            if (txtValue == "") {
+                alertError.innerHTML = "Ingrese un numero de expediente.";
+                alertError.style.display = "block";
+                return false
+            } else {
+                alertError.style.display = "none";
+                
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ RegenerateCover.aspx.cs
@@ -0,0 +1,173 @@
+﻿using Qexpeditive.BusinessLayer.Messages.Dossiers;
+using Qexpeditive.Web.BackendActions;
+using Qframework.Common.Exceptions;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System;
+using System.Linq;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Threading;
+
+public partial class RegenerateCover_RegenerateCover : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+  
+    protected void SubmitRegenerate_Click(object sender, EventArgs e)
+    {
+        string retAlert = ValidateExpNumber();
+        if (retAlert != "OK")
+        {
+            alertSucces.Style.Add("display","none");
+            alertError.InnerHtml = retAlert;
+            alertError.Style.Add("display", "block");
+        }
+        else
+        {
+            alertError.Style.Add("display", "none");
+            alertSucces.Style.Add("display", "block");
+        }
+        nroExpediente.Text = "";
+    }
+    
+    private string ValidateExpNumber()
+    {
+        string msg = "OK";
+        var nroExpedienteFormato = nroExpediente.Text.Split('/');
+        if (nroExpedienteFormato.Count() == 2)
+        {
+            int aux;
+            var nroExp = nroExpediente.Text.Split('/')[0];
+            var anioExp = nroExpediente.Text.Split('/')[1];
+            if (Int32.TryParse(nroExp, out aux) && Int32.TryParse(anioExp, out aux))
+            {
+                string dossierID = "";
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["QExpeditive"].ToString()))
+                {
+                    con.Open();
+                    using (SqlCommand command = new SqlCommand("SELECT DossierID " +
+                                                                "FROM Dossier " +
+                                                                "WHERE DossierGlobalRelativeID = @nroExp and YEAR(StartDate) = @anioExp;"
+                                                                , con))
+                    {
+                        command.Parameters.Add("@nroExp", System.Data.SqlDbType.VarChar);
+                        command.Parameters["@nroExp"].Value = nroExp;
+                        command.Parameters.Add("@anioExp", System.Data.SqlDbType.Int);
+                        command.Parameters["@anioExp"].Value = anioExp;
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            reader.Read();
+                            if (reader.HasRows)
+                            {
+                                dossierID = reader.GetGuid(0).ToString();
+                                if (!VerifyQueued(dossierID))
+                                {
+                                    this.RegenerateCover(dossierID);
+                                }
+                                else
+                                {
+                                    msg = "La carátula ya se encuentra en el proceso de regeneración.";
+                                }
+                            }
+                            else
+                            {
+                                msg = "El trámite que ha ingresado no existe.";
+                            }
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            else
+            {
+                msg = "El trámite ingresado no tiene un formato correcto (Ej. 0001/2017)";
+            }
+        }
+        else 
+        {
+            msg = "El trámite ingresado no tiene un formato correcto (Ej. 0001/2017)";
+        }
+        return msg;
+    }
+
+    
+    private void RegenerateCover(string dossierID)
+    {
+        using (SqlConnection con  = new SqlConnection(ConfigurationManager.ConnectionStrings["QExpeditive"].ToString()))
+        {
+            var query = "EXEC RegeneratePDF @DossierId=@DossierId2, @ActuationId=NULL";
+            using (SqlCommand command = new SqlCommand(query, con))
+            {
+                con.Open();
+                command.Parameters.AddWithValue("@DossierId2", SqlDbType.UniqueIdentifier).Value = dossierID;
+                command.ExecuteNonQuery();
+                con.Close();
+                this.WaitRegenerate(dossierID);
+            }
+        }
+    }
+
+    private void WaitRegenerate(string dossierID)
+    {
+        bool validate = false;
+        using (SqlConnection con  = new SqlConnection( ConfigurationManager.ConnectionStrings["QExpeditive"].ToString()))
+        {
+            con.Open();
+            using (SqlCommand command = new SqlCommand("SELECT [DossierID]"+
+                                                        "FROM [CoverToPrint]"+
+                                                        "WHERE DossierID = @DossierId",con))
+            {
+                command.Parameters.AddWithValue("@DossierId", SqlDbType.UniqueIdentifier);
+                command.Parameters["@DossierId"].Value = dossierID;
+                while (validate == false)
+                {
+                    Thread.Sleep(5000);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (!reader.Read())
+                        {
+                            validate = true;
+                        }
+                    }
+                }
+            }
+            con.Close();
+        }
+    }
+
+    private bool VerifyQueued(string dossierID) //Verifica que no exista una caratula regenerandose.
+    {
+        var verify = false;
+        using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["QExpeditive"].ToString()))
+        {
+            con.Open();
+            using (SqlCommand command = new SqlCommand("SELECT [DossierID]" +
+                                                        "FROM [CoverToPrint]" +
+                                                        "WHERE DossierID = @DossierId", con))
+            {
+                command.Parameters.AddWithValue("@DossierId", SqlDbType.UniqueIdentifier);
+                command.Parameters["@DossierId"].Value = dossierID;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        verify = true;
+                    }
+                    else
+                    {
+                        verify = false;
+                    }
+                }
+            }
+            con.Close();
+        }
+        return verify;
+    }
+}
